ac68e9288cb33fa6908f53debbe8927176216962
LemonAdd <lemon.add0@gmail.com>
2019-01-19 15:22:57 +0100

    Design connexion

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Home</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
-    <link rel="stylesheet" href="css/prism.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
     <!-- <link rel="stylesheet" href="css/debug.css"> -->
     <link rel="stylesheet" href="css/main.css">
--- login.include.php
+++ login.include.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="css/debug.css"> -->
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body style="background-color: #303952; background-image: url(img/geo.png);">
 <?php
 /**
  * Created by PhpStorm.
@@ -39,11 +51,47 @@ elseif(isset($_POST['pseudo']) && isset($_POST['psd'])){ // si il se connecte av
 }
 
 echo <<<HTML
-    <form action="#" method="post">
+    <!--<form action="#" method="post">
         <label>Identifiant(Pseudo) : <input type="text" name="pseudo" required></label>
         <label>Mot de passe : <input type="password" name="psd" required></label>
         <label>Remember me :<input type="checkbox" name="remember"> </label>
         <input type="submit">
-    </form>
-HTML;
+    </form> -->
+    <section class="hero is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                    <h3 class="title" style="color:#3dc1d3;">Se connecter</h3>
+                    <p class="subtitle" style="color: #A2A9BD;">Merci de vous connecter pour entrer dans ce monde merveilleux...</p>
+                        <form action="#" method="post">
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input is-rounded has-text-centered" type="text" name="pseudo" placeholder="Pseudo" autofocus="" required>
+                                </div>
+                            </div>
 
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input is-rounded has-text-centered" type="password" name="psd" placeholder="Mot de passe" required>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <label class="checkbox has-text-white">
+                                  <input type="checkbox" name="remember">
+                                  Se souvenir de moi
+                                </label>
+                            </div>
+                            <button type="submit" class="button is-block is-fullwidth is-rounded" style="background-color:#3dc1d3; color: white; border: none;">Se connecter</button>
+                        </form>
+                    <p class="has-text-grey" style="margin-top: 20px;">
+                        <a href="inscription.include.php">Inscription</a> &nbsp;·&nbsp;
+                        <a href="#">Mot de passe oublié</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+HTML;
+?>
+</body>
+</html>
