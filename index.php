355a8b233ee51cdad5e4299d299dcca692100263
LemonAdd <lemon.add0@gmail.com>
2019-01-19 01:36:03 +0100

    Accueil v1 terminé

--- index.php
+++ index.php
@@ -6,19 +6,14 @@
     <title>Home</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
     <link rel="stylesheet" href="css/prism.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
     <!-- <link rel="stylesheet" href="css/debug.css"> -->
     <link rel="stylesheet" href="css/main.css">
-    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
-    <style type="text/css">
-    	.hero{
-    		background: linear-gradient(rgba(0, 0, 0, 0.45), rgba(0, 0, 0, 0.45)), url(img/bg.png);
-    	}
-    </style>
   </head>
   <body>
 
 <!-- .hero -->
-<section class="hero">
+<section class="hero is-dark is-shadowless is-medium">
 
 <!-- .hero-head -->
 <div class="hero-head">
@@ -27,9 +22,9 @@
 			<p class="navbar-item has-text-white">BetterSmash</p>
 		</div>
 		<div class="column center desktop">
-			<p class="navbar-item"><a href="#" class="nonactive">GUIDES</a></p>
-			<p class="navbar-item active">ACCUEIL</p>
-			<p class="navbar-item"><a href="#" class="nonactive">COMBATTANTS</a></p>
+			<p class="navbar-item"><a href="#" class="nonactive"><i class="fas fa-book" style="color:#3dc1d3;margin-right: 5px;"></i>GUIDES</a></p>
+			<p class="navbar-item active"><i class="fas fa-home" style="margin-right: 5px;"></i> ACCUEIL</p>
+			<p class="navbar-item"><a href="#" class="nonactive"><i class="fas fa-fist-raised" style="color:#3dc1d3;margin-right: 5px;"></i>COMBATTANTS</a></p>
 		</div>
 		<div class="column right">
 			<p class="navbar-item has-text-white desktop">CONNEXION</p>
@@ -41,44 +36,147 @@
 	</div>
 </div>
 <!-- /.hero-head -->
+
+<!-- .hero-body -->
+<div class="hero-body" style="background-color: #303952; background-image: url(img/geo.png);">
+	<div class="container has-text-centered">		
+		<h1 class="title toggleadd is-1 is-size-1-touch">&mdash; Better<span style="color:#3dc1d3;">Smash</span> &mdash;</h1>
+		<h2 class="title">Ne perdez plus de temps, <span style="color:#3dc1d3;">créez</span> ou <span style="color:#3dc1d3;">trouvez</span> votre guide !</h2>
+		<a class="button is-primary is-rounded is-large has-text-weight-bold wobble-hor-bottom" style="text-transform: uppercase; background-color: #3dc1d3">Commencer dès maintenant</a>
+	</div>
+</div>
+<!-- /.hero-body -->
+
+<!-- /.hero-foot -->
+<div class="hero-foot" style="background-color: #3dc1d3">
+	<div class="container has-text-centered">
+		<div class="columns is-centered" style="margin-top: -40px;padding-bottom: 20px;">
+			<div class="column is-3">
+				<span class="icon is-large rotating">
+				  <i class="fas fa-pencil-ruler fa-7x"></i>
+				</span>
+				<h3 class="title" style="margin-top: 40px;">Créez vos propres guides</h3>
+				<p class="heading has-text-weight-bold" style="font-size: 15px;">Vous voulez partager votre expérience de jeu ? Lancez vous! Sur BetterSmash, peu importe votre niveau, partagez vos techniques à tout le monde!</p>
+			</div>
+			<div class="column is-3">
+				<span class="icon is-large rotating">
+				  <i class="fab fa-readme fa-7x"></i>
+				</span>
+				<h3 class="title" style="margin-top: 40px;">Consulter et améliorer</h3>
+				<p class="heading has-text-weight-bold" style="font-size: 15px;">BetterSmash est avant tout une communauté. Apprenez des meilleurs, mais aidez aussi les plus novices. N'hésitez pas à proposer des améliorations à certains guides!</p>
+			</div>
+			<div class="column is-3">
+				<span class="icon is-large rotating">
+				  <i class="fas fa-running fa-7x"></i>
+				</span>
+				<h3 class="title" style="margin-top: 40px;">Découvrez les combattants du moment!</h3>
+				<p class="heading has-text-weight-bold" style="font-size: 15px;">Sur BetterSmash, la Tier liste est mise à jour régulièrement par nos <s>grands</s> spécialistes <s>de l'esport</s></p>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- /.hero-foot -->
+
 </section>
 <!-- /.hero -->
 
-<section class="hero">
+<!-- button -->
+<div class="has-text-centered" style="margin-top: -20px;">
+	<a class="button is-rounded jello-horizontal" style="color: white; border: none; background-color: #303952;">Qu'est-ce que vous attendez ? Inscrivez vous !</a>
+</div>
+
+<!-- .news -->
+<div class="container">
+	<div class="titre">
+		<h1 class="texte title is-4">Derniers articles de la rédaction</h1>
+	</div>
+
+	<div class="columns">
+		<div class="column is-3">
+			<div class="article">
+				<a href="#" class="article-link">
+					<div class="article-img" style="background: linear-gradient(rgba(86, 192, 224, 0.45), rgba(86, 192, 224, 0.45)),url(img/bg.png); background-position: center center;background-repeat: no-repeat;background-size: cover;"></div>
+				</a>
+				<div class="article-content">
+					<h2 class="title is-3" style="color:#3dc1d3;">Bienvenue lol</h2>
+					<p style="margin-top: -20px;" class="is-dark">par LemonAdd</p>
+				</div>
+			</div>
+		</div>
+		<div class="column is-3">
+			<div class="article">
+				<a href="#" class="article-link">
+					<div class="article-img" style="background: linear-gradient(rgba(86, 192, 224, 0.45), rgba(86, 192, 224, 0.45)),url(img/bg.png); background-position: center center;background-repeat: no-repeat;background-size: cover;"></div>
+				</a>
+				<div class="article-content">
+					<h2 class="title is-3" style="color:#3dc1d3;">Bienvenue lol</h2>
+					<p style="margin-top: -20px;" class="is-dark">par LemonAdd</p>
+				</div>
+			</div>
+		</div>
+		<div class="column is-3">
+			<div class="article">
+				<a href="#" class="article-link">
+					<div class="article-img" style="background: linear-gradient(rgba(86, 192, 224, 0.45), rgba(86, 192, 224, 0.45)),url(img/bg.png); background-position: center center;background-repeat: no-repeat;background-size: cover;"></div>
+				</a>
+				<div class="article-content">
+					<h2 class="title is-3" style="color:#3dc1d3;">Bienvenue lol</h2>
+					<p style="margin-top: -20px;" class="is-dark">par LemonAdd</p>
+				</div>
+			</div>
+		</div>
+		<div class="column is-3">
+			<div class="article">
+				<a href="#" class="article-link">
+					<div class="article-img" style="background: linear-gradient(rgba(86, 192, 224, 0.45), rgba(86, 192, 224, 0.45)),url(img/bg.png); background-position: center center;background-repeat: no-repeat;background-size: cover;"></div>
+				</a>
+				<div class="article-content">
+					<h2 class="title is-3" style="color:#3dc1d3;">Bienvenue lol</h2>
+					<p style="margin-top: -20px;" class="is-dark">par LemonAdd</p>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+
+	<div class="has-text-centered">
+		<a class="button is-rounded is-medium" style="color: white; border: none; background-color: #3dc1d3;">Voir tous les articles</a>
+	</div>
+
+</div>
+<!-- /.news -->
+
+
+<!-- .footer -->
+<section class="hero" style="background-color: #303952; color:#A2A9BD; margin-top: 50px;">
   <div class="hero-body">
     <div class="container">
-      <h1 class="title has-text-centered">
-        Lorem ipsum
-      </h1>
-      <h2 class="subtitle has-text-centered">
-        Yo
-      </h2>
+      <div class="columns">
+      	<div class="column is-4">
+      		<h1 class="title heading" id="title">BetterSmash</h1>
+      		<p style="margin-top: -20px;"><i class="fab fa-twitter-square"></i> <i class="fab fa-discord"></i></p>
+      	</div>
+      	<div class="column is-2">
+      		<h1 class="title heading is-footer" id="title">Membre</h1>
+      		<p class="heading is-footer">connexion</p>
+      		<p class="heading is-footer">inscription</p>
+      	</div>
+      	<div class="column is-2">
+      		<h1 class="title heading is-footer" id="title">Guides</h1>
+      		<p class="heading is-footer">liste</p>
+      		<p class="heading is-footer">créer un guide</p>
+      	</div>
+      	<div class="column is-2">
+      		<h1 class="title heading is-footer" id="title">Combattants</h1>
+      		<p class="heading is-footer">liste</p>
+      		<p class="heading is-footer">tier list</p>
+      	</div>
+      </div>
+      <div class="has-text-centered">&copy; 2019 &mdash; Mentions légales</div>
     </div>
   </div>
 </section>
-
-<!-- .hero -->
-<section class="hero">
-
-<!-- .hero-head -->
-<div class="hero-head">
-	<div class="columns is-mobile is-marginless heading has-text-weight-bold" style="background-color: #596275;">
-
-		<div class="column center">
-			<p class="navbar-item"><a href="#" class="nonactive">Lorem ispum</a></p>
-			<p class="navbar-item"><a href="#" class="nonactive">Lorem ispum</a></p>
-			<p class="navbar-item"><a href="#" class="nonactive">Lorem ipsum</a></p>
-		</div>
-
-	</div>
-</div>
-<!-- /.hero-head -->
-</section>
-<!-- /.hero -->
-
-<p> oiuiuou ouiuouo</p>
-
-
+<!-- /.footer -->
   	
   </body>
 </html>
