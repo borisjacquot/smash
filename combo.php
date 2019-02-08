c7218a7411f4a4bacb8dd69af849c523ac947a2f
Clement Millard <clement.millard@etudiant.univ-reims.fr>
2019-02-08 23:16:13 +0100

    code combo fini mais a ameliorer

---
+++ combo.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+<div id="choice">
+    <span onclick="selectButton('A')">A</span>
+    <span onclick="selectButton('B')">B</span>
+    <span onclick="selectButton('X')">X</span>
+    <span onclick="selectButton('Y')">Y</span>
+    <span onclick="selectButton('UP')">UP</span>
+    <i class="fas fa-caret-up"></i>
+
+    <span onclick="selectButton('DOWN')">DOWN</span>
+    <span onclick="selectButton('LEFT')">LEFT</span>
+    <span onclick="selectButton('RIGHT')">RIGHT</span>
+    <span onclick="selectButton(',')">RIGHT</span>
+    <span onclick="selectButton('+')">RIGHT</span>
+</div>
+<div id="result">
+
+</div>
+
+<script>
+    function selectButton(String button){
+        var result = document.getElementById("result");
+        var image = doc.createElement(img);
+        img.setAttribute("src",)
+        result.appendChild(doc.createElement(img).
+    }
+</script>
